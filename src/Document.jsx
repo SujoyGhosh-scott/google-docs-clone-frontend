4b3db24eb5f60d036bd230a8e63a93db9f3c5105
Sujoy Ghosh <60333881+SujoyGhosh-scott@users.noreply.github.com>
2021-09-12 21:06:03 +0530

    .

--- src/Document.jsx
+++ src/Document.jsx
@@ -13,7 +13,7 @@ export default function Document() {
   const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001");
+    const s = io("https://sg-google-docs-clone.herokuapp.com");
     setSocket(s);
 
     return () => {
